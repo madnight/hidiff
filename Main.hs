ac13a11c6a6107e9b1a8886e219b832f53f9b8e1
Fabian Beuke <mail@beuke.org>
2018-08-19 22:37:30 +0200

    unparse

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RecordWildCards #-}
+
 
 module Main where
 
@@ -7,16 +8,30 @@ import Control.Monad
 import Data.Either
 import Data.Maybe
 import Data.Text (Text, pack, unpack, unlines)
-import Prelude (IO, putStrLn, print,(.), ($), (<$>), error, concat)
+import Prelude (IO, putStrLn, print,(.), ($), (<$>), error, concat, Show, show)
 import System.IO
 import Text.Diff.Parse
 import Text.Diff.Parse.Types
 import Text.Highlighting.Pygments
 
-parseDiff' (Right r) = r
-parseDiff' (Left err) = error err
+
+diffErrorHandler (Right r) = r
+diffErrorHandler (Left err) = error err
 
 parseContent (Hunks xs) = (fmap . fmap ) lineContent  (hunkLines <$> xs)
+
+unparseLine :: Line -> Text
+unparseLine = lineContent
+
+unparseHunk :: Hunk -> Text
+unparseHunk x = unlines $ unparseLine <$> (hunkLines x)
+
+unparseContent :: Content -> Text
+unparseContent Binary = pack ""
+unparseContent (Hunks xs) = unlines $ unparseHunk <$> xs
+
+unparseDiff :: FileDelta -> Text
+unparseDiff model@FileDelta {..} = unparseContent fileDeltaContent
 
 main :: IO ()
 main = do
@@ -25,14 +40,16 @@ main = do
    let hl = highlight hsLexer terminalFormatter [("encoding", "utf-8")]
    contents <- readFile "test.patch"
 
-   print
-      $  unpack
-     <$> fileDeltaSourceFile
-     <$> (parseDiff' . parseDiff $ pack contents)
+   {- print -}
+      {- $  unpack -}
+     {- <$> fileDeltaSourceFile -}
+     {- <$> (diffErrorHandler . parseDiff $ pack contents) -}
 
-   (mapM_ . mapM_) (\x -> hl (concat x) >>= putStr)
-      $  (fmap . fmap . fmap) unpack (parseContent
-     <$> (fileDeltaContent <$> (parseDiff' $ parseDiff $ pack contents)))
+   {- (mapM_ . mapM_) (\x -> hl (concat x) >>= putStr) -}
+      {- $  (fmap . fmap . fmap) unpack (parseContent -}
+     {- <$> (fileDeltaContent <$> (diffErrorHandler $ parseDiff $ pack contents))) -}
 
-   print $ parseDiff' $ parseDiff $ pack contents
+   let a = diffErrorHandler $ parseDiff $ pack contents
+   print $ unparseDiff <$> a
+   {- print $ unparseDiff -}
    putStrLn "Hello, Haskell!"
