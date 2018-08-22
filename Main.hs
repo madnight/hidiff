dbc14ff1532d1008f7a32e9cbb7d5535e515f23c
Fabian Beuke <mail@beuke.org>
2018-08-22 02:02:33 +0200

    cleanup

--- Main.hs
+++ Main.hs
@@ -1,52 +1,42 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE RecordWildCards           #-}
+{-# LANGUAGE TemplateHaskell           #-}
 
 module Main where
 
-import Control.Arrow
-import Control.Lens hiding ((|>))
-import Control.Monad
-import Data.Either
-import Data.Maybe
-import Data.Text (Text, pack, unpack, unlines)
-import Data.Text.IO
-import Prelude hiding (unlines, putStrLn, readFile)
-import System.IO.Unsafe
-import Text.Diff.Parse
+import Control.Lens
+import Data.Text (Text, pack, unlines, unpack)
+import Data.Text.IO (putStrLn, readFile)
+import Prelude hiding (putStrLn, readFile, unlines)
+import Text.Diff.Parse (parseDiff)
 import Text.Diff.Parse.Types
-import Text.Highlighting.Pygments
+import qualified Text.Highlighting.Pygments as P
 
 makeLensesFor [("fileDeltaContent", "fD")] ''FileDelta
-makeLensesFor [("hunkLines", "hL")]        ''Hunk
-makeLensesFor [("lineContent", "lC")]      ''Line
-makePrisms                                 ''Content
+makeLensesFor [("hunkLines", "hL")] ''Hunk
+makeLensesFor [("lineContent", "lC")] ''Line
+makePrisms ''Content
 
-diffErrorHandler :: Either String p -> p
-diffErrorHandler (Right r) = r
-diffErrorHandler (Left err) = error err
+highlight :: Text -> Text -> IO Text
+highlight name str = do
+  Just lexer <- P.getLexerByName $ unpack name
+  pack <$> P.highlight lexer P.terminalFormatter encoding (unpack str)
+  where
+    encoding = [("encoding", "utf-8")]
 
-hl :: Text -> Text -> IO Text
-hl name str = do
-      Just hsLexer <- getLexerByName (unpack name)
-      x <- highlight hsLexer
-         terminalFormatter [("encoding", "utf-8")] (unpack str)
-      pure $ pack x
+contentLens :: Applicative f => (Text -> f Text) -> FileDelta -> f FileDelta
+contentLens = fD . _Hunks . ix 0 . hL . ix 0 . lC
 
-setContentFileDelta :: Text -> FileDelta -> FileDelta
-setContentFileDelta str fileDelta = fileDelta
-     & fD . _Hunks . ix 0 . hL . ix 0 . lC .~ str
+setContent :: Text -> FileDelta -> FileDelta
+setContent str fileDelta = fileDelta & contentLens .~ str
 
 main :: IO ()
 main = do
-   let getter = fD . _Hunks . ix 0 . hL . ix 0 . lC
-
-   contents <- readFile "test.patch"
-
-   let b = head $ diffErrorHandler $ parseDiff contents
-   let x = b ^. getter
-
-   p <- hl "hs" x
-
-   putStrLn $ setContentFileDelta p b ^. getter
+  contents <- readFile "test.patch"
+  let diff = head $ diffError $ parseDiff contents
+  hiContent <- highlight "hs" $ diff ^. contentLens
+  putStrLn $ setContent hiContent diff ^. contentLens
+  where
+    diffError (Right r) = r
+    diffError (Left _)  = error "diff parsing exception: no a valid git diff"
